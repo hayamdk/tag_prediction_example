1209d87dfb29aa07dfd461044213ad13072aeff4
haya <hayasoft@hotmail.com>
2016-02-10 21:46:00 +0900

    python3に対応するように書き換え

--- predict_tag.py
+++ predict_tag.py
@@ -1,12 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import niconico_chainer_models
 import pickle
 import argparse
-import urllib2
+import urllib.request
 import numpy
 import PIL.Image
 import chainer
+
+def fix_b_to_str(model) :
+	if hasattr(model, "__dict__"):
+		list = model.__dict__.copy()
+		for d in list:
+			d_str = d.decode('utf8')
+			model.__dict__[d_str] = model.__dict__[d]
+			del model.__dict__[d]
+			fix_b_to_str(model.__dict__[d_str])
+
 def fetch_image(url):
-    response = urllib2.urlopen(url)
+    response = urllib.request.urlopen(url)
     image = numpy.asarray(PIL.Image.open(response).resize((224,224)), dtype=numpy.float32)
     if (not len(image.shape)==3): # not RGB
         image = numpy.dstack((image, image, image))
@@ -32,17 +45,21 @@ if args.gpu >= 0:
 else:
     xp = numpy
 
-model = pickle.load(open(args.model))
+f = open(args.model, 'rb')
+model = pickle.load(f, fix_imports=True, encoding='bytes')
+fix_b_to_str(model)
+
 if args.gpu >= 0:
     model.to_gpu()
 
-mean_image = numpy.load(open(args.mean))
-tags = [line.rstrip() for line in open(args.tags)]
+mean_image = numpy.load(open(args.mean, 'rb'), encoding='bytes')
+tags = [line.rstrip() for line in open(args.tags, encoding='utf-8')]
 tag_dict = dict((i,tag) for i, tag in enumerate(tags))
 
 img_preprocessed = (to_bgr(fetch_image(args.image_url)) - mean_image).transpose((2, 0, 1))
 
 predicted = model.predict(xp.array([img_preprocessed]))[0]
+#predicted = model.predict_all(xp.array([img_preprocessed]))
 
 top_10 = sorted(enumerate(predicted), key=lambda index_value: -index_value[1])[:30]
 top_10_tag = [
